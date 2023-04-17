371ff825a43c7a6949aab6ccb34b25bf7c781fba
Yifang Zhang <zhang303@illinois.edu>
2023-04-16 19:41:15 -0500

    updated for english version

---
+++ transcripts.py
@@ -0,0 +1,72 @@
+# importing libraries 
+import speech_recognition as sr 
+import os 
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
+
+# create a speech recognition object
+r = sr.Recognizer()
+
+def convert_m4a_to_wav(path):
+    sound = AudioSegment.from_file(path, "m4a")
+    path = path[:-4]
+    sound.export(path + ".wav", format="wav")
+
+# a function that splits the audio file into chunks
+# and applies speech recognition
+def get_large_audio_transcription(path):
+    """
+    Splitting the large audio file into chunks
+    and apply speech recognition on each of these chunks
+    """
+    # open the audio file using pydub
+    sound = AudioSegment.from_wav(path)  
+    # split audio sound where silence is 700 miliseconds or more and get chunks
+    chunks = split_on_silence(sound,
+        # experiment with this value for your target audio file
+        min_silence_len = 500,
+        # adjust this per requirement
+        silence_thresh = sound.dBFS-14,
+        # keep the silence for 1 second, adjustable as well
+        keep_silence=500,
+    )
+    folder_name = "audio-chunks"
+    # create a directory to store the audio chunks
+    if not os.path.isdir(folder_name):
+        os.mkdir(folder_name)
+    else:
+        # remove all files in the audio-chunks directory
+        for filename in os.listdir(folder_name):
+            os.remove(folder_name + "/" + filename)
+    whole_text = ""
+    # process each chunk 
+    for i, audio_chunk in enumerate(chunks, start=1):
+        # export audio chunk and save it in
+        # the `folder_name` directory.
+        chunk_filename = os.path.join(folder_name, f"chunk{i}.wav")
+        audio_chunk.export(chunk_filename, format="wav")
+        # recognize the chunk
+        with sr.AudioFile(chunk_filename) as source:
+            audio_listened = r.record(source)
+            # try converting it to text
+            try:
+                text = r.recognize_google(audio_listened)
+            except sr.UnknownValueError as e:
+                print("Error:", str(e))
+            else:
+                text = f"{text.capitalize()}. "
+                print(chunk_filename, ":", text)
+                whole_text += text
+    # return the text for all chunks detected
+    return whole_text
+
+
+# main function
+if __name__ == '__main__':
+    if(os.path.exists("create_character.wav")):
+        print("wav file already exists")
+    else:
+        print("converting m4a to wav")
+        convert_m4a_to_wav("create_character.m4a")
+    text = get_large_audio_transcription("create_character.wav")
+    print("\nFull text:", text)
